b16857e13df2e38b13505ccf850f02e7a58b1bac
Shubham <shubhamlahase@hmail.com>
2023-01-26 23:55:27 +0530

    connected to database, installed some packages, created user schema model and register route

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const User = require('./models/User');
 const app = express();
-const PORT = 4000;
+
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 6000;
+const uri = process.env.MONGO_URI;
+
+mongoose.set('strictQuery', true);
+mongoose.connect(uri, () => { console.log('connected to database') })
 
 app.get('/test', (req,res) => {
     res.json('express server test ~ OK');
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running at localhost:${PORT}`);
+app.post('/Register', async (req,res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.create({username, password})
+    res.json(userDoc);
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at localhost:${port}`);
 });
