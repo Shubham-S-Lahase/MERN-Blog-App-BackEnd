ffc5bcfa5cde1cfeee1534b0305cc29e453721d8
Shubham <shubhamlahase@hmail.com>
2023-02-09 11:05:02 +0530

    added route for view the specific full post

--- server/routes/post.js
+++ server/routes/post.js
@@ -45,4 +45,11 @@ router.get('/get-post', async (req,res) => {
     );
 });
 
+router.get('/post/:id', async (req,res) => {
+     const {id} = req.params;
+    const postDoc = await Post.findById(id)
+    .populate('author', ['username']);
+    res.json(postDoc);
+})
+
 module.exports = router;
